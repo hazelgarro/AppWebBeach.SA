69c484f5abbf2820eead855dc9d8f04c6eb30d83
artaviafloresmonica <artaviafloresmonica@gmail.com>
2024-02-22 17:52:05 -0600

    Se agregó el controlador de reservaciones y la conexion al API local

---
+++ AppWebBeachSA/Controllers/ReservacionesController.cs
@@ -0,0 +1,41 @@
+﻿using AppWebBeachSA.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace AppWebBeachSA.Controllers
+{
+    public class ReservacionesController : Controller
+    {
+        private HotelAPI hotelAPI;
+
+        private HttpClient client;
+
+        /// <summary>
+        /// Metodo constructor
+        /// </summary>
+        public ReservacionesController()
+        {
+            hotelAPI = new HotelAPI();
+
+            client = hotelAPI.Initial();
+        }
+
+        public async Task<IActionResult> Listado()
+        {
+            List<Reservacion> listado = new List<Reservacion>();
+
+            HttpResponseMessage response = await client.GetAsync("/Reservaciones/ListaReservas");
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var resultados = response.Content.ReadAsStringAsync().Result;
+
+                listado = JsonConvert.DeserializeObject<List<Reservacion>>(resultados);
+            }
+
+            return View(listado);
+        }
+
+    }
+}
---
+++ AppWebBeachSA/Models/HotelAPI.cs
@@ -0,0 +1,18 @@
+﻿namespace AppWebBeachSA.Models
+{
+    public class HotelAPI
+    {
+
+        public HttpClient Initial()
+        {
+            //objeto HttpClient
+            var client = new HttpClient();
+
+            //URL API
+            client.BaseAddress = new Uri("https://localhost:7094/");
+
+            return client;
+        }
+
+    }
+}
